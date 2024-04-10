4617f654044c05e923de510394a8eee9d6a9a560
Asif Minar <asifminar007@gmail.com>
2024-04-11 02:44:29 +0600

    input

--- basic.py
+++ basic.py
@@ -11,3 +11,18 @@ num = 10.5
 print(type(num))
 num = "this is not a number"
 print(type(num))
+
+
+#input
+
+'''
+this is a
+multiline
+comment
+'''
+
+inp = input("Enter a float number: ")
+print(type(inp))
+numeric_inp = float(inp)
+print(type(numeric_inp))
+print("Given user input:", inp)
