51e27f7facd7209daa6a2f2ad963a25f2840bc2d
Asif Minar <asifminar7@gmail.com>
2024-05-24 02:07:16 +0600

    fibonacci

--- basic.py
+++ basic.py
@@ -329,3 +329,16 @@ MinMax(arr)
 
 
 '''
+
+# fibonacci
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(2,n):
+        c = a + b
+        a = b
+        b = c
+        print(c)
+
+fibonacci(5)
