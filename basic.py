c6ebf76f8468a399b9395a379ece9a298cf850dc
Asif Minar <asifminar7@gmail.com>
2024-05-24 22:29:06 +0600

    fibonacci series

--- basic.py
+++ basic.py
@@ -330,15 +330,20 @@ MinMax(arr)
 
 '''
 
-# fibonacci
+# fibonacci series
 
 def fibonacci(n):
     a = 0
     b = 1
-    for i in range(2,n):
-        c = a + b
-        a = b
-        b = c
-        print(c)
+    if n==1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
 
-fibonacci(5)
+fibonacci(10)
