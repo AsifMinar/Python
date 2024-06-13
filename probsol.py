abdcf8d6d645918414b3d4b501b59bed172094f2
Asif Minar <asifminar7@gmail.com>
2024-06-14 02:18:20 +0600

    leetcode_p3

--- probsol.py
+++ probsol.py
@@ -59,7 +59,7 @@ print(ts([2,7,11,15],9))
 
 
 
-'''
+
 
 # Define the Solution class
 class Solution:
@@ -93,3 +93,45 @@ print(solution.isPalindrome(10))  # Expected output: False
 
 
 
+'''
+
+class Solution:
+    def romanToInt(self, s):
+        # Create a dictionary to map Roman numerals to their respective integer values
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        # Initialize the result variable
+        result = 0
+        
+        # Iterate through the Roman numeral string
+        for i in range(len(s)):
+            # Get the integer value of the current Roman numeral
+            value = roman_to_int[s[i]]
+            
+            # If the current value is less than the next value, subtract it from the result
+            if i < len(s) - 1 and value < roman_to_int[s[i + 1]]:
+                result -= value
+            # Otherwise, add it to the result
+            else:
+                result += value
+        
+        return result
+
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Test cases
+print(solution.romanToInt("III"))  # Expected output: 3
+print(solution.romanToInt("IV"))  # Expected output: 4
+print(solution.romanToInt("IX"))  # Expected output: 9
+print(solution.romanToInt("LVIII"))  # Expected output: 58
+print(solution.romanToInt("MCMXCIV"))  # Expected output: 1994
