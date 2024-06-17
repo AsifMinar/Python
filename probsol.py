0078ea7eab3de46049801a1e0f13127e229a08cc
Asif Minar <asifminar7@gmail.com>
2024-06-18 01:16:02 +0600

    leetcode_p7

--- probsol.py
+++ probsol.py
@@ -213,7 +213,7 @@ print(solution.isValid(input6))  # Output: False
 
 
 
-'''
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -273,3 +273,30 @@ while current:
     print(current.val, "->", end=" ")
     current = current.next
 print("None")
+
+
+'''
+
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+        
+        unique = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[unique - 1]:
+                nums[unique] = nums[i]
+                unique += 1
+
+        return unique
+
+
+# Example usage
+nums = [1, 1, 2, 2, 3, 3, 4, 5, 5]
+solution = Solution()
+length = solution.removeDuplicates(nums)
+
+print("Original Array:", nums)
+print("Length:", length)
+print("Modified Array:", nums[:length])
