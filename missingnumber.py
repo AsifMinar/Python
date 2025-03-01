16cb232e9e620fc383689843db5e77e1eb0e4a73
asif minar <asifminar7@gmail.com>
2025-03-02 02:13:27 +0600

    missingnumber

---
+++ missingnumber.py
@@ -0,0 +1,33 @@
+'''
+n = [1,2]
+n.sort()
+
+def missing_number(n):
+  for i in range(len(n)):
+    if i != n[i]:
+      if n[0] == 1:
+        f = i
+        break
+      f = i
+      break
+    else:
+      f = i+1
+  return f
+
+print(missing_number(n))
+
+'''
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)):
+            if i != nums[i]:
+                if nums[0] == 1:
+                    f = i
+                    break
+                f = i
+                break
+            else:
+                f = i+1
+        return f
